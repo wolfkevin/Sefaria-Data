6eecd3b010c53862b61cfb522dcc8cedd4f84dbc
Kevin <kevwolf@umich.edu>
2018-12-19 13:50:11 +0200

    better re

--- research/self_link_creator/fix_'".py
+++ research/self_link_creator/fix_'".py
@@ -11,8 +11,8 @@ for idx in IndexSet():
     title_changed = False
     for title in idx.all_titles(lang='he'):
         new_title = re.sub(ur'([\u0590-\u05FF])([\u2018\u2019\u0027][\u2018\u2019\u0027]|[\"“”])([\u0590-\u05FF])', ur'\1״\3', title)
-        new_title = re.sub(ur'[\u2018\u2019\u0027][\u2018\u2019\u0027]', ur'"', new_title)
-        new_title = re.sub(ur'[\u2018\u2019\u0027]', ur'׳', new_title)        
+        new_title = re.sub(ur'([\u2018\u2019\u0027][\u2018\u2019\u0027]|[“”])', ur'"', new_title)
+        new_title = re.sub(ur'[\u2018\u2019]', ur'׳', new_title)        
         if title != new_title:
             print(title)
             print(new_title)
